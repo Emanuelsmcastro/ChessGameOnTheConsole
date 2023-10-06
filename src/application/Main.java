a0eb3929a07ab506106a92c49b35484f3251fdc2
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-06 14:00:48 -0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
