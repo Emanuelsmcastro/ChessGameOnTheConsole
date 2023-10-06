37b2466b0ee4958e10f80cb453babf9a09d153e3
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-06 15:23:01 -0300

    Add new colors

--- src/chess/Color.java
+++ src/chess/Color.java
@@ -1,7 +1,11 @@
 package chess;
 
 public enum Color {
-	BLACK("\u001B[30m"), WHITE("\u001B[37m"), RESET("\u001B[0m");
+	BLACK("\u001B[30m"),
+	WHITE("\u001B[37m"),
+	BLUE("\u001B[34m"),
+	RED("\u001B[31m"),
+	RESET("\u001B[0m");
 
 	private String color;
 
