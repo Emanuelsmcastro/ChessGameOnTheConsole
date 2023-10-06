7c901200d96ddb716e4f2a152d478a2c2e79adf0
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-06 14:50:07 -0300

    Update toString method

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -12,7 +12,7 @@ public class Rook extends ChessPiece {
 
 	@Override
 	public String toString() {
-		return PieceSymbol.HOOK.getValue();
+		return getColor().getValue() + PieceSymbol.HOOK.getValue() + Color.RESET.getValue();
 	}
 	
 	
