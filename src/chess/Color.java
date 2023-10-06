6ff2a2698304d1b7fc1bd4ad6a94ff6ff0d57f5e
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-06 14:49:44 -0300

    Update Color.java

--- src/chess/Color.java
+++ src/chess/Color.java
@@ -1,6 +1,17 @@
 package chess;
 
 public enum Color {
-	BLACK,
-	WHITE;
+	BLACK("\u001B[30m"),
+	WHITE("\u001B[37m"),
+	RESET("\u001B[0m");
+	
+	private String color;
+	
+	Color(String color){
+		this.color = color;
+	}
+	
+	public String getValue() {
+		return color;
+	}
 }
