e75396bf48d1e24fd353369cda3a2ffcc46ffc76
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-06 14:23:10 -0300

    Create PieceSymbol.java

---
+++ src/chess/pieces/PieceSymbol.java
@@ -0,0 +1,16 @@
+package chess.pieces;
+
+public enum PieceSymbol {
+	HOOK("♜"),
+	KING("♚");
+	
+	private String value;
+	
+	PieceSymbol(String value){
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+}
