31724ccc6a178a2295359a9e0830772edc2c5765
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-06 22:39:24 -0300

    Possibles moves to rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -31,4 +31,8 @@ public class Position {
 		this.column = column;
 	}
 
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,7 +29,8 @@ public class ChessMatch {
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
-		validadeSourcePosition(source);
+		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -41,11 +42,16 @@ public class ChessMatch {
 		return capturedPiece;
 	}
 	
-	private void validadeSourcePosition(Position position) {
+	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position))
 			throw new ChessException("There is no piece on source position");
 		if(!board.piece(position).isThereAnyPossibleMove())
 			throw new ChessException("There is no possible moves for the chosen piece.");
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target))
+			throw new ChessException("The chosen piece can't move to target position.");
 	}
 
 	private void initialSetup() {
