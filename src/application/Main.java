56c80c0a0bd9c750c84576c5c16920d8e312f0ab
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-07 20:31:47 -0300

    Create special move Promotion

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,11 +13,11 @@ import chess.ChessPosition;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
+
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
@@ -35,8 +35,13 @@ public class Main {
 				ChessPosition target = UI.readChessPosition(scanner);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				if(capturedPiece != null)
+				if (capturedPiece != null)
 					captured.add(capturedPiece);
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = scanner.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				scanner.nextLine();
