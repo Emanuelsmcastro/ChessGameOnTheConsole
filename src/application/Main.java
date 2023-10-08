1b4079e8d877ef32b84de5b21b8a6300e6fa70ea
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-07 22:43:59 -0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -40,6 +40,10 @@ public class Main {
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
 					String type = scanner.nextLine().toUpperCase();
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Enter piece for promotion (B/N/R/Q): ");
+						type = scanner.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			} catch (ChessException e) {
